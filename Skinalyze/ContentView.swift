8196aad735da19e13501aa6b75c689bd44bddb47
Alejandro Ollivier <aleollivierochoa@gmail.com>
2024-11-07 15:54:31 +0100

    Initial Commit

---
+++ Skinalyze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skinalyze
+//
+//  Created by Alejandro Ollivier Ochoa on 07/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skinalyze/SkinalyzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinalyzeApp.swift
+//  Skinalyze
+//
+//  Created by Alejandro Ollivier Ochoa on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SkinalyzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
